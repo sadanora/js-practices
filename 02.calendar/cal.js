368c78621fe28d50c51e6bcec16e368d8cd806db
sadanora <sadanora.pi@gmail.com>
2022-06-30 12:12:49 +0900

    clean: 変数名を変更

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -5,7 +5,6 @@ dayjs.locale('ja')
 
 // コマンドライン引数の取得
 const now = dayjs()
-
 const argv = require('minimist')(process.argv.slice(2), {
   alias: {
     y: 'year',
@@ -18,31 +17,36 @@ const argv = require('minimist')(process.argv.slice(2), {
 })
 
 // 対象月のインスタンス作成
-const targerMonth = dayjs(`${argv.y}-${argv.m}`)
+const targetMonth = dayjs(`${argv.y}-${argv.m}`)
 
 // 日付の取得
-const startDate = targerMonth.startOf('month')
-const endDate = targerMonth.endOf('month')
+const startDate = targetMonth.startOf('month')
+const endDate = targetMonth.endOf('month')
 const dates = []
-
 for (let date = startDate; date <= endDate; date = date.add(1, 'd')) {
   dates.push(date)
 }
 
 // ヘッダーを表示
-let headerPadding = 0
-targerMonth.format('MMMM YYYY').length === 8 ? headerPadding = 14 : headerPadding = 13
-console.log(targerMonth.format('MMMM YYYY').padStart(headerPadding, ' ').padEnd(20, ' '))
+const PaddingRight = 20
+let PaddingLeft = 0
+targetMonth.format('MMMM YYYY').length === 8 ? PaddingLeft = 14 : PaddingLeft = 13
+
+console.log(targetMonth.format('MMMM YYYY').padStart(PaddingLeft, ' ').padEnd(PaddingRight, ' '))
 console.log(('日 月 火 水 木 金 土'))
 
 // 1日までを空白で埋める
+const blankUnit = 3
 const wday = startDate.format('d')
-process.stdout.write(`${' '.repeat(wday * 3)}`)
+
+process.stdout.write(`${' '.repeat(wday * blankUnit)}`)
 
 // 日付の表示
+const saturday = 6
+
 for (const date of dates) {
   process.stdout.write(`${date.format('D').padStart(2, ' ')} `)
-  if (parseInt(date.format('d')) === 6) {
+  if (parseInt(date.format('d')) === saturday) {
     process.stdout.write('\n')
   }
 }
