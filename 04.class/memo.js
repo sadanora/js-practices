a9d5f6c661f441163be988b77a699753346eef1e
sadanora <sadanora.pi@gmail.com>
2022-07-15 17:30:13 +0900

    eslint

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -1,29 +1,29 @@
 const fs = require('fs')
-const enquirer = require('enquirer');
-const minimist = require('minimist');
-const readline = require('readline');
+const enquirer = require('enquirer')
+const minimist = require('minimist')
+const readline = require('readline')
 
 // ディレクトリが存在しなければ作成
 const directoryPath = './memos'
 if (!fs.existsSync(directoryPath)) {
-  fs.mkdirSync(directoryPath);
+  fs.mkdirSync(directoryPath)
 }
 
 // 標準入力の受け取り
 const parseStdin = async () => {
   const buffers = []
-  for await (const chunk of process.stdin) buffers.push(chunk);
-  const text = String.prototype.concat(buffers);
+  for await (const chunk of process.stdin) buffers.push(chunk)
+  const text = String.prototype.concat(buffers)
   return text
-};
+}
 
 // メモの追加
 const createMemo = () => {
   parseStdin().then(text => {
     const fileName = text.split('\n')[0]
-    fs.writeFileSync(`./memos/${fileName}.txt`, text);
-  });
-};
+    fs.writeFileSync(`./memos/${fileName}.txt`, text)
+  })
+}
 
 // メモの一覧表示
 const generateChoices = () => {
@@ -35,23 +35,23 @@ const generateChoices = () => {
   return choices
 }
 
-const reference = (async ()=> {
+const reference = async () => {
   const question = {
     type: 'select',
     name: 'chooseMemo',
     message: 'Choose a memo you want to see:',
     choices: generateChoices()
-  };
-  const answer = await enquirer.prompt(question);
+  }
+  const answer = await enquirer.prompt(question)
 
   const stream = fs.createReadStream(`./memos/${answer.chooseMemo}.txt`, 'utf8')
   const rl = readline.createInterface({
-    input: stream,
-  });
+    input: stream
+  })
 
   rl.on('line', (lineString) => {
     console.log(lineString)
-  });
-});
+  })
+}
 
 reference()
