ac44b74c5d448f64e35254d409ed90095be46559
sadanora <sadanora.pi@gmail.com>
2022-07-17 13:56:49 +0900

    clean: main関数を他の関数に合わせてarrow関数にしたetc
    ディレクトの作成もmain内で行うようにした
    argvの処理はclass宣言より後ろの位置に移動した

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -7,25 +7,6 @@ const readline = require('readline')
 // メモの保存先ディレクトリを宣言
 const DIRECTORY_PATH = './memos'
 
-// ディレクトリが存在しなければ作成
-if (!fs.existsSync(DIRECTORY_PATH)) {
-  fs.mkdirSync(DIRECTORY_PATH)
-}
-
-// オプションの受け取り
-const argv = minimist(process.argv.slice(2), {
-  alias: {
-    l: 'list',
-    r: 'refer',
-    d: 'delete'
-  },
-  default: {
-    l: false,
-    r: false,
-    d: false
-  }
-})
-
 class Memo {
   // constructor() {
   // }
@@ -87,10 +68,27 @@ class Memo {
   }
 }
 
+// オプションの受け取り
+const argv = minimist(process.argv.slice(2), {
+  alias: {
+    l: 'list',
+    r: 'refer',
+    d: 'delete'
+  },
+  default: {
+    l: false,
+    r: false,
+    d: false
+  }
+})
+
 // メインロジック
 const memo = new Memo()
-
-function main (argv) {
+const main = (argv) => {
+  // ディレクトリが存在しなければ作成
+  if (!fs.existsSync(DIRECTORY_PATH)) {
+    fs.mkdirSync(DIRECTORY_PATH)
+  }
   if (argv.r) {
     memo.refer()
   } else if (argv.l) {
